fb24850fa1c71e45f0d3e3798725cef80fb4fc40
Administrator <1315702128@qq.com>
2019-01-15 19:52:14 +0800

    Initial commit

---
+++ app/src/main/java/com/tssss/bysj/contract/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.tssss.bysj.contract;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
